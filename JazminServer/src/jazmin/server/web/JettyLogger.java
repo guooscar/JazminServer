d532ac6e41d30224e3644ebd4a85a00f7495f9a5
yama <guooscar@gmail.com>
2015-04-01 14:00:12 +0800

    Update JettyLogger.java

--- JazminServer/src/jazmin/server/web/JettyLogger.java
+++ JazminServer/src/jazmin/server/web/JettyLogger.java
@@ -5,405 +5,86 @@ package jazmin.server.web;
 
 import jazmin.log.LoggerFactory;
 
-import org.apache.logging.log4j.Level;
+import org.eclipse.jetty.util.log.AbstractLogger;
 import org.eclipse.jetty.util.log.Logger;
 
 /**
- * @author yama
- * 28 Dec, 2014
+ * @author yama 28 Dec, 2014
  */
-public class JettyLogger implements Logger{
-	/**
-	 * @return
-	 * @see jazmin.log.Logger#getName()
-	 */
+public class JettyLogger extends AbstractLogger {
+	private final static jazmin.log.Logger logger = LoggerFactory
+			.get(JettyLogger.class);
+
+	public JettyLogger() throws Exception {
+
+	}
+
 	public String getName() {
 		return logger.getName();
 	}
 
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#debug(java.lang.Object, java.lang.Throwable)
-	 */
-	public void debug(Object arg0, Throwable arg1) {
-		logger.debug(arg0, arg1);
+	public void warn(String msg, Object... args) {
+		logger.warn(msg, args);
 	}
 
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#debug(java.lang.Object)
-	 */
-	public void debug(Object arg0) {
-		logger.debug(arg0);
+	public void warn(Throwable thrown) {
+		warn("", thrown);
 	}
 
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#debug(java.lang.String, java.lang.Object[])
-	 */
-	public void debug(String arg0, Object... arg1) {
-		logger.debug(arg0, arg1);
+	public void warn(String msg, Throwable thrown) {
+		logger.warn(msg, thrown);
 	}
 
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#debug(java.lang.String, java.lang.Throwable)
-	 */
-	public void debug(String arg0, Throwable arg1) {
-		logger.debug(arg0, arg1);
+	public void info(String msg, Object... args) {
+		logger.info(msg, args);
 	}
 
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#debug(java.lang.String)
-	 */
-	public void debug(String arg0) {
-		logger.debug(arg0);
+	public void info(Throwable thrown) {
+		info("", thrown);
 	}
 
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#error(java.lang.Object, java.lang.Throwable)
-	 */
-	public void error(Object arg0, Throwable arg1) {
-		logger.error(arg0, arg1);
+	public void info(String msg, Throwable thrown) {
+		logger.info(msg, thrown);
 	}
 
-	/**
-	 * @return
-	 * @see java.lang.Object#hashCode()
-	 */
-	public int hashCode() {
-		return logger.hashCode();
+	public void debug(String msg, Object... args) {
+		logger.debug(msg, args);
 	}
 
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#error(java.lang.Object)
-	 */
-	public void error(Object arg0) {
-		logger.error(arg0);
+	public void debug(String msg, long arg) {
+		if (isDebugEnabled())
+			logger.debug(msg, new Object[] { new Long(arg) });
 	}
 
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#error(java.lang.String, java.lang.Object[])
-	 */
-	public void error(String arg0, Object... arg1) {
-		logger.error(arg0, arg1);
+	public void debug(Throwable thrown) {
+		debug("", thrown);
 	}
 
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#error(java.lang.String, java.lang.Throwable)
-	 */
-	public void error(String arg0, Throwable arg1) {
-		logger.error(arg0, arg1);
+	public void debug(String msg, Throwable thrown) {
+		logger.debug(msg, thrown);
 	}
 
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#error(java.lang.String)
-	 */
-	public void error(String arg0) {
-		logger.error(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#fatal(java.lang.Object, java.lang.Throwable)
-	 */
-	public void fatal(Object arg0, Throwable arg1) {
-		logger.fatal(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#fatal(java.lang.Object)
-	 */
-	public void fatal(Object arg0) {
-		logger.fatal(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#fatal(java.lang.String, java.lang.Object[])
-	 */
-	public void fatal(String arg0, Object... arg1) {
-		logger.fatal(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#fatal(java.lang.String, java.lang.Throwable)
-	 */
-	public void fatal(String arg0, Throwable arg1) {
-		logger.fatal(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#fatal(java.lang.String)
-	 */
-	public void fatal(String arg0) {
-		logger.fatal(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#info(java.lang.Object, java.lang.Throwable)
-	 */
-	public void info(Object arg0, Throwable arg1) {
-		logger.info(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#info(java.lang.Object)
-	 */
-	public void info(Object arg0) {
-		logger.info(arg0);
-	}
-
-	/**
-	 * @param obj
-	 * @return
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	public boolean equals(Object obj) {
-		return logger.equals(obj);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#info(java.lang.String, java.lang.Object[])
-	 */
-	public void info(String arg0, Object... arg1) {
-		logger.info(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#info(java.lang.String, java.lang.Throwable)
-	 */
-	public void info(String arg0, Throwable arg1) {
-		logger.info(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#info(java.lang.String)
-	 */
-	public void info(String arg0) {
-		logger.info(arg0);
-	}
-
-	/**
-	 * @return
-	 * @see jazmin.log.Logger#isDebugEnabled()
-	 */
 	public boolean isDebugEnabled() {
 		return logger.isDebugEnabled();
 	}
 
-	/**
-	 * @param arg0
-	 * @return
-	 * @see jazmin.log.Logger#isEnabled(org.apache.logging.log4j.Level)
-	 */
-	public boolean isEnabled(Level arg0) {
-		return logger.isEnabled(arg0);
+	public void setDebugEnabled(boolean enabled) {
+		warn("setDebugEnabled not implemented", null, null);
 	}
 
 	/**
-	 * @return
-	 * @see jazmin.log.Logger#isErrorEnabled()
+	 * Create a Child Logger of this Logger.
 	 */
-	public boolean isErrorEnabled() {
-		return logger.isErrorEnabled();
+	protected Logger newLogger(String fullname) {
+		return this;
 	}
 
-	/**
-	 * @return
-	 * @see jazmin.log.Logger#isFatalEnabled()
-	 */
-	public boolean isFatalEnabled() {
-		return logger.isFatalEnabled();
+	public void ignore(Throwable ignored) {
+		logger.warn(ignored);
 	}
 
-	/**
-	 * @return
-	 * @see jazmin.log.Logger#isInfoEnabled()
-	 */
-	public boolean isInfoEnabled() {
-		return logger.isInfoEnabled();
-	}
-
-	/**
-	 * @return
-	 * @see jazmin.log.Logger#isTraceEnabled()
-	 */
-	public boolean isTraceEnabled() {
-		return logger.isTraceEnabled();
-	}
-
-	/**
-	 * @return
-	 * @see jazmin.log.Logger#isWarnEnabled()
-	 */
-	public boolean isWarnEnabled() {
-		return logger.isWarnEnabled();
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#trace(java.lang.Object, java.lang.Throwable)
-	 */
-	public void trace(Object arg0, Throwable arg1) {
-		logger.trace(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#trace(java.lang.Object)
-	 */
-	public void trace(Object arg0) {
-		logger.trace(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#trace(java.lang.String, java.lang.Object[])
-	 */
-	public void trace(String arg0, Object... arg1) {
-		logger.trace(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#trace(java.lang.String, java.lang.Throwable)
-	 */
-	public void trace(String arg0, Throwable arg1) {
-		logger.trace(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#trace(java.lang.String)
-	 */
-	public void trace(String arg0) {
-		logger.trace(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#warn(java.lang.Object, java.lang.Throwable)
-	 */
-	public void warn(Object arg0, Throwable arg1) {
-		logger.warn(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#warn(java.lang.Object)
-	 */
-	public void warn(Object arg0) {
-		logger.warn(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#warn(java.lang.String, java.lang.Object[])
-	 */
-	public void warn(String arg0, Object... arg1) {
-		logger.warn(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 * @see jazmin.log.Logger#warn(java.lang.String, java.lang.Throwable)
-	 */
-	public void warn(String arg0, Throwable arg1) {
-		logger.warn(arg0, arg1);
-	}
-
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#warn(java.lang.String)
-	 */
-	public void warn(String arg0) {
-		logger.warn(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 * @see jazmin.log.Logger#catching(java.lang.Throwable)
-	 */
-	public void catching(Throwable arg0) {
-		logger.catching(arg0);
-	}
-
-	/**
-	 * @return
-	 * @see java.lang.Object#toString()
-	 */
+	@Override
 	public String toString() {
 		return logger.toString();
 	}
-
-	private static jazmin.log.Logger logger=LoggerFactory.get(JettyLogger.class);
-
-	@Override
-	public void debug(Throwable arg0) {
-		logger.debug(arg0);
-	}
-
-	@Override
-	public void debug(String arg0, long arg1) {
-		logger.debug(arg0,arg1);
-	}
-
-	@Override
-	public Logger getLogger(String arg0) {
-		return null;
-	}
-
-	@Override
-	public void ignore(Throwable arg0) {
-		
-	}
-
-	@Override
-	public void info(Throwable arg0) {
-		logger.info(arg0);
-	}
-
-	@Override
-	public void warn(Throwable arg0) {
-		logger.warn(arg0);
-	}
-
-	@Override
-	public void setDebugEnabled(boolean arg0) {
-		
-	}
-	
 }
