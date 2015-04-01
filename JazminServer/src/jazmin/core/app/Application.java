7c163ce1e47dce47866192f08ff1633e7f5e8e48
yama <guooscar@gmail.com>
2015-04-01 10:43:25 +0800

    Update Application.java

--- JazminServer/src/jazmin/core/app/Application.java
+++ JazminServer/src/jazmin/core/app/Application.java
@@ -4,8 +4,14 @@
 package jazmin.core.app;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import jazmin.core.Driver;
 import jazmin.core.Jazmin;
@@ -31,7 +37,8 @@ public class Application extends Lifecycle {
 	@SuppressWarnings("unchecked")
 	public <T> T create(Class<T>clazz)throws Exception{
 		T instance =clazz.newInstance();
-		for(Field f:clazz.getDeclaredFields()){
+		for(Field f:getField(clazz)){
+			System.err.println(f);
 			if(f.isAnnotationPresent(AutoWired.class)){
 				f.setAccessible(true);
 				Class<?>fieldType=f.getType();
@@ -52,6 +59,7 @@ public class Application extends Lifecycle {
 						logger.warn("can not find autowired server:"+fieldType);
 					}
 				}else{
+					System.err.println("set up :"+fieldType);
 					Object target=autoWiredMap.get(fieldType);
 					if(target==null){
 						target=create(fieldType);
@@ -62,5 +70,23 @@ public class Application extends Lifecycle {
 			}
 		}
 		return instance;
+	}
+	//
+	private Set<Field> getField(Class<?>clazz){
+		HashSet<Field>result=new HashSet<Field>();
+		result.addAll(Arrays.asList(clazz.getDeclaredFields()));
+		if(clazz.getSuperclass()!=null){
+			getParentField(clazz,result);
+		}
+		return result;
+	}
+	
+	private void getParentField(Class<?>clazz,Set<Field>fields){
+		Class<?>superClass=clazz.getSuperclass();
+		if(clazz.getSuperclass()==null){
+			return;
+		}
+		fields.addAll(Arrays.asList(superClass.getDeclaredFields()));
+		getParentField(superClass, fields);
 	}
 }
