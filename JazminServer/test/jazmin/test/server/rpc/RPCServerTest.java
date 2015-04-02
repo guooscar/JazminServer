3fd6cd379e1e60bc1a50163c92c9b62351df3ac7
yama <guooscar@gmail.com>
2015-04-02 10:45:00 +0800

    Update RPCServerTest.java

--- JazminServer/test/jazmin/test/server/rpc/RPCServerTest.java
+++ JazminServer/test/jazmin/test/server/rpc/RPCServerTest.java
@@ -13,10 +13,10 @@ import jazmin.server.rpc.RPCServer;
 public class RPCServerTest {
 	//
 	public static void main(String[] args) throws Exception{
-		Jazmin.start();
 		RPCServer rpcServer=new RPCServer();
-		rpcServer.init();
-		rpcServer.start();
+		rpcServer.credential("123");
+		Jazmin.addServer(rpcServer);
+		Jazmin.start();
 		//
 		rpcServer.registerService(new TestRemoteServiceImpl());
 	}
