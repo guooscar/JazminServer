3f4a47a0b35753c56fec4bd9a0b6d732bba0df1d
yama <guooscar@gmail.com>
2015-04-02 10:44:47 +0800

    Update RPCClientTest.java

--- JazminServer/test/jazmin/test/server/rpc/RPCClientTest.java
+++ JazminServer/test/jazmin/test/server/rpc/RPCClientTest.java
@@ -3,6 +3,9 @@
  */
 package jazmin.test.server.rpc;
 
+import jazmin.core.Jazmin;
+import jazmin.driver.rpc.JazminRPCDriver;
+
 
 /**
  * @author yama
@@ -11,6 +14,10 @@ package jazmin.test.server.rpc;
 public class RPCClientTest {
 	//
 	public static void main(String[] args) throws Exception{
-		
+		JazminRPCDriver driver=new JazminRPCDriver();
+		driver.addRemoteServer("1","1", "localhost", 6001);
+		Jazmin.addDriver(driver);
+		Jazmin.start();
+		driver.create(TestRemoteService.class,"1").timeoutMethod();
 	}
 }
