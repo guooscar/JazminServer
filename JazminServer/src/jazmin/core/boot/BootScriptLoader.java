24918ba3aa82f7226f6c6eed82c4230b77ab5f1f
guooscar <guooscar@gmail.com>
2015-05-22 17:56:39 +0800

    add https support for StartupContext

--- JazminServer/src/jazmin/core/boot/BootScriptLoader.java
+++ JazminServer/src/jazmin/core/boot/BootScriptLoader.java
@@ -4,8 +4,15 @@
 package jazmin.core.boot;
 
 import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -27,6 +34,32 @@ import jazmin.util.IOUtil;
  * 27 Dec, 2014
  */
 public class BootScriptLoader {
+	static {
+	    final TrustManager[] trustAllCertificates = new TrustManager[] {
+	        new X509TrustManager() {
+	            @Override
+	            public X509Certificate[] getAcceptedIssuers() {
+	                return null; // Not relevant.
+	            }
+	            @Override
+	            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+	                // Do nothing. Just allow them all.
+	            }
+	            @Override
+	            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+	                // Do nothing. Just allow them all.
+	            }
+	        }
+	    };
+
+	    try {
+	        SSLContext sc = SSLContext.getInstance("SSL");
+	        sc.init(null, trustAllCertificates, new SecureRandom());
+	        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	    } catch (GeneralSecurityException e) {
+	        throw new ExceptionInInitializerError(e);
+	    }
+	}
 	//
 	private InputStream inputStream;
 	public BootScriptLoader(InputStream input) {
