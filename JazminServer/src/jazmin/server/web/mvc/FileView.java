c5f8d54391d93e5bb102233efb8f2e3625efa0af
yama <guooscar@gmail.com>
2016-01-17 01:14:38 +0800

    add cache control to FileView

--- JazminServer/src/jazmin/server/web/mvc/FileView.java
+++ JazminServer/src/jazmin/server/web/mvc/FileView.java
@@ -5,6 +5,7 @@ package jazmin.server.web.mvc;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Date;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
@@ -18,13 +19,20 @@ import jazmin.util.IOUtil;
 public class FileView implements View{
 	private File file;
 	private String mimetype;
+	private int cacheSeconds;
 	public FileView(String filePath,String mimetype) {
 		this.file=new File(filePath);
 		this.mimetype=mimetype;
 	}
+	public FileView(String filePath,String mimetype,int cacheSeconds) {
+		this.file=new File(filePath);
+		this.mimetype=mimetype;
+		this.cacheSeconds=cacheSeconds;
+	}
 	public FileView(String filePath) {
 		this(filePath,null);
 	}
+	
 	//
 	@Override
 	public void render(Context ctx) throws Exception {
@@ -47,6 +55,12 @@ public class FileView implements View{
         String filename = new String(file.getName().getBytes("UTF-8"), "ISO8859-1");
         response.setHeader("Content-Disposition", 
         		"attachment; filename=\"" + filename + "\"");
+        if(cacheSeconds>0){
+        	long expiry = new Date().getTime() + cacheSeconds*1000;
+        	response.setDateHeader("Expires", expiry);
+        	response.setHeader("Cache-Control", "max-age="+ cacheSeconds);
+        }
+        response.addHeader( "Cache-Control", "public, max-age=90000" );
         FileInputStream fis=new FileInputStream(file);
         IOUtil.copy(fis,outStream);
         IOUtil.closeQuietly(fis);
