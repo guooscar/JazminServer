264550895821f7f1695d0374f4496ae3c5a3662c
yama <guooscar@gmail.com>
2016-08-18 19:20:41 +0800

    upgrade jetty to 9.3

--- JazminServer/test/jazmin/test/server/web/TestWebServer.java
+++ JazminServer/test/jazmin/test/server/web/TestWebServer.java
@@ -3,6 +3,9 @@
  */
 package jazmin.test.server.web;
 
+import jazmin.core.Jazmin;
+import jazmin.driver.file.FileServerDriver;
+import jazmin.driver.rpc.JazminRpcDriver;
 import jazmin.server.web.WebServer;
 
 /**
@@ -15,11 +18,24 @@ public class TestWebServer {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception{
-		WebServer ws=new WebServer();
-		ws.addResource("/","/Users/yama/Documents/JavaWorkspace/ITCircleWebSystem/release/ITCircleWebSystem");
-		ws.init();
-		ws.start();
 		//
+		Jazmin.setServerName("boss"+System.getProperty("user.name"));
+		JazminRpcDriver rpcDriver=new JazminRpcDriver();
+		rpcDriver.addRemoteServer("YcytBizSystem","app","skydu.local", 6003);
+		//rpcDriver.addRemoteServer("jazmin://wx.yicyt.com:8601/"+YcytBizSystem.class.getSimpleName()+"/app1");
+		Jazmin.addDriver(rpcDriver);
+		//
+		FileServerDriver fileServerDriver=new FileServerDriver();
+		fileServerDriver.setHomeDir("local_file_storage");
+		fileServerDriver.addServer("YcytFileSystem","wx.yicyt.com",8604,1);
+		Jazmin.addDriver(fileServerDriver);
+		//
+		WebServer ws=new WebServer();
+		//ws.setEnableJettyLogger(true);
+		ws.addResource("/","/Users/yama/Documents/JavaWorkspace/YcytBossSystem/release/YcytBossSystem");
+		Jazmin.addServer(ws);
+		ws.setPort(7003);
+		Jazmin.start();
 		
 	}
 
