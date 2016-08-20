c16083a4206ce3083e9f7251b83b2b6b9d449d79
icecooly <icecooly.du@qq.com>
2016-08-20 13:02:23 +0800

    fix bug

--- JazminDeployer/src/jazmin/deploy/view/console/CommandHistoryWindow.java
+++ JazminDeployer/src/jazmin/deploy/view/console/CommandHistoryWindow.java
@@ -54,7 +54,7 @@ public class CommandHistoryWindow extends Window{
 			@Override
 			public void itemClick(ItemClickEvent event) {
 				WebBrowser wb=Page.getCurrent().getWebBrowser();
-				//support ios android
+				//support ios and android
 				if(wb.isIPhone()||wb.isAndroid()||event.isDoubleClick()){
 					HistoryBean hb=table.getValueByItem(event.getItem());
 					selectedCommand=hb.cmd;
