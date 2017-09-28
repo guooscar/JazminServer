ef47ddad6a1f7f540e69a1e324af4cdfb264153c
yama <guooscar@gmail.com>
2017-09-29 00:02:16 +0800

    message queue

--- JazminServer/test/jazmin/test/driver/mq/MessageQueueTest.java
+++ JazminServer/test/jazmin/test/driver/mq/MessageQueueTest.java
@@ -18,7 +18,7 @@ import jazmin.util.RandomUtil;
  */
 public class MessageQueueTest {
 	public static void main(String[] args) {
-		LoggerFactory.setLevel("INFO");
+		LoggerFactory.setLevel("ERROR");
 		MessageQueueDriver mq=new MessageQueueDriver();
 		Jazmin.addDriver(mq);
 		ConsoleServer cs=new ConsoleServer();
@@ -47,8 +47,8 @@ public class MessageQueueTest {
 		Scanner sc=new Scanner(System.in);
 		while(true){
 			sc.nextInt();
-			byte payload[]=new byte[RandomUtil.randomInt(1000,2000)];
-			for(int i=0;i<RandomUtil.randomInt(7000, 2000000);i++)
+			byte payload[]=new byte[RandomUtil.randomInt(500,2000)];
+			for(int i=0;i<RandomUtil.randomInt(70000, 20000000);i++)
 			mq.publish("test1", payload);
 		}
 		
