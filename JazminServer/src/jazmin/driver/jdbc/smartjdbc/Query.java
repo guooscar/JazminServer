f98fb8e07d2cc7c077b759dfefd2dc53245ca19b
skydu <icecooly.du@qq.com>
2018-03-24 13:42:30 +0800

    add

--- JazminServer/src/jazmin/driver/jdbc/smartjdbc/Query.java
+++ JazminServer/src/jazmin/driver/jdbc/smartjdbc/Query.java
@@ -16,12 +16,20 @@ public class Query {
 	public int pageIndex;//从1开始
 	
 	@QueryField(ingore=true)
-	public int pageSize;
+	public int pageSize=20;
 	
 	@QueryField(ingore=true)
 	public Integer orderType;
 	//
+	public static Integer defaultOrderType=null;
+	public static Integer defaultPageSize=20;
+	//
 	public Query(){
+		orderType=defaultOrderType;
+		pageIndex=1;
+		if(defaultPageSize!=null) {
+			pageSize=defaultPageSize;
+		}
 	}
 	//
 	public int getStartPageIndex(){
