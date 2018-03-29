bbe7efb0e7cfa80e3a1cca20d608ea8f42c27237
skydu <icecooly.du@qq.com>
2018-03-29 20:07:20 +0800

    suport class

--- JazminServer/src/jazmin/driver/jdbc/smartjdbc/SmartDAO.java
+++ JazminServer/src/jazmin/driver/jdbc/smartjdbc/SmartDAO.java
@@ -6,9 +6,11 @@ import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.Blob;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.Timestamp;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -363,6 +365,35 @@ public class SmartDAO extends JazminDAO{
 			throws Exception {
 		convertBean(o, null, rs, excludeFields);
 	}
+	///
+	protected static final HashSet<Class<?>> WRAP_TYPES=new HashSet<>();
+	static{
+		WRAP_TYPES.add(Boolean.class);
+		WRAP_TYPES.add(Character.class);
+		WRAP_TYPES.add(Byte.class);
+		WRAP_TYPES.add(Short.class);
+		WRAP_TYPES.add(Integer.class);
+		WRAP_TYPES.add(Long.class);
+		WRAP_TYPES.add(BigDecimal.class);
+		WRAP_TYPES.add(BigInteger.class);
+		WRAP_TYPES.add(Double.class);
+		WRAP_TYPES.add(Float.class);
+		WRAP_TYPES.add(String.class);
+		WRAP_TYPES.add(Date.class);
+		WRAP_TYPES.add(Timestamp.class);
+		WRAP_TYPES.add(java.sql.Date.class);
+		WRAP_TYPES.add(Byte[].class);
+		WRAP_TYPES.add(byte[].class);
+		WRAP_TYPES.add(int.class);
+		WRAP_TYPES.add(boolean.class);
+		WRAP_TYPES.add(char.class);
+		WRAP_TYPES.add(byte.class);
+		WRAP_TYPES.add(short.class);
+		WRAP_TYPES.add(int.class);
+		WRAP_TYPES.add(long.class);
+		WRAP_TYPES.add(float.class);
+		WRAP_TYPES.add(double.class);
+	}
 	/**
 	 * 
 	 * @param o
@@ -399,7 +430,9 @@ public class SmartDAO extends JazminDAO{
 				continue;
 			}
 			if(!columnNames.contains(fieldName)) {
-				continue;
+				if(WRAP_TYPES.contains(fieldType)){
+					continue;
+				}
 			}
 			Object value = null;
 			if (fieldType.equals(String.class)) {
