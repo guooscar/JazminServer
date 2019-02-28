b21ebb700daedbb47f59f5ea15bac315f5a39a9b
skydu <icecooly.du@qq.com>
2019-02-28 15:29:11 +0800

    add

--- JazminServer/src/jazmin/server/web/WebRequestLog.java
+++ JazminServer/src/jazmin/server/web/WebRequestLog.java
@@ -1,12 +1,14 @@
 package jazmin.server.web;
 
-import jazmin.log.Logger;
-import jazmin.log.LoggerFactory;
+import javax.servlet.http.HttpServletRequest;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.RequestLog;
 import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.util.component.AbstractLifeCycle;
+
+import jazmin.log.Logger;
+import jazmin.log.LoggerFactory;
 
 /**
  * 
@@ -27,7 +29,7 @@ public class WebRequestLog extends AbstractLifeCycle implements RequestLog {
 		}
 		if (logger.isInfoEnabled()) {
 			StringBuilder sb = new StringBuilder();
-			sb.append('[').append(req.getRemoteHost()).append(':')
+			sb.append('[').append(getRealIpAddress(req)).append(':')
 					.append(req.getRemotePort()).append("] ");
 			sb.append(req.getProtocol() + " ");
 			sb.append("[").append(req.getMethod()).append("] ");
@@ -41,5 +43,23 @@ public class WebRequestLog extends AbstractLifeCycle implements RequestLog {
 			}
 			logger.info(sb.toString());
 		}
+	}
+	
+	public static String getRealIpAddress(HttpServletRequest request) {
+	    String ip = request.getHeader("x-forwarded-for");
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+	        ip = request.getHeader("Proxy-Client-IP");
+	    }
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+	        ip = request.getHeader("WL-Proxy-Client-IP");
+	    }
+	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+	        ip = request.getRemoteAddr();
+	    }
+	    if (ip.contains(",")) {
+	        return ip.split(",")[0];
+	    } else {
+	        return ip;
+	    }
 	}
 }
