14c797470fe41e4d04e025fcf081318c60b7fa09
icecooly <icecooly.du@qq.com>
2020-07-03 21:21:17 +0800

    add

--- JazminServer/src/jazmin/core/app/ApplicationLoader.java
+++ JazminServer/src/jazmin/core/app/ApplicationLoader.java
@@ -5,6 +5,7 @@ package jazmin.core.app;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.jar.JarEntry;
@@ -12,6 +13,7 @@ import java.util.jar.JarInputStream;
 
 import jazmin.log.Logger;
 import jazmin.log.LoggerFactory;
+import jazmin.util.FileUtil;
 import jazmin.util.IOUtil;
 
 /**
@@ -40,22 +42,31 @@ public class ApplicationLoader {
 			throw new IllegalArgumentException("can not find application image:"
 					+applicationPackage);
 		}
+		String applicationName=applicationPackage.getName();
+		applicationName=applicationName.substring(0,applicationName.length()-4);
+		//
 		try {
 			workImage=new File(workDir,applicationPackage.getName());
-			IOUtil.copyFile(applicationPackage, workImage);
+			IOUtil.copyFile(applicationPackage, workImage);	
+			File applicationWorkDir=new File(workDir+File.separator+applicationName);
+			if(applicationWorkDir.exists()) {
+				if(applicationWorkDir.isFile()) {
+					applicationWorkDir.delete();
+				}else {
+					FileUtil.deleteDirectory(applicationWorkDir);
+				}
+			}
+			FileUtil.unzip(workImage.getAbsolutePath(),applicationWorkDir.getAbsolutePath());
 		} catch (Exception e) {
 			logger.error(e.getMessage(),e);
 			return null;
 		}
 		//
-		String applicationName=applicationPackage.getName();
-		//extra .jar
-		applicationName=applicationName.substring(0,applicationName.length()-4);
 		//
 		for(Class<?>clz:getApplicationClasses()){
 			try {
 				if(clz.getSimpleName().equals(applicationName)){
-						return (Application) clz.newInstance();
+					return (Application) clz.newInstance();
 				}
 			} catch (Throwable e) {
 				logger.warn(e.getMessage());
@@ -80,23 +91,32 @@ public class ApplicationLoader {
 		if(workImage==null){
 			return appClass;
 		}
+		JazminClassloader appClassLoader=null;
 		try (JarInputStream jis=new JarInputStream(
 					new FileInputStream(workImage))){
 			JarEntry entry;
-			AppClassloader appClassLoader=new AppClassloader(workImage);
+			appClassLoader=new JazminClassloader(workImage);
 			while((entry=jis.getNextJarEntry())!=null){
 				String name=entry.getName();
 				if(name.endsWith(".class")){
 					String className=name.replace('/', '.');
 					className=className.substring(0,className.length()-6);
 					try{
-						appClass.add(Class.forName(className,false,appClassLoader));
+						appClass.add(appClassLoader.loadClass(className));
 					}catch(Throwable e){
+						logger.error(e.getMessage(),e);
 					}
 				}
 			}
 		} catch (Exception e) {
 			logger.error(e.getMessage(),e);
+		}finally {
+			if(appClassLoader!=null) {
+				try {
+					appClassLoader.close();
+				} catch (IOException e) {
+				}
+			}
 		}
 		return appClass;
 	}
---
+++ JazminServer/src/jazmin/core/app/JazminClassloader.java
@@ -0,0 +1,69 @@
+package jazmin.core.app;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+import jazmin.log.Logger;
+import jazmin.log.LoggerFactory;
+
+/**
+ * 
+ * @author yama 4 Jan, 2015
+ */
+public class JazminClassloader extends URLClassLoader {
+	//
+	private static Logger logger=LoggerFactory.get(JazminClassloader.class);
+	public JazminClassloader(File workImage) throws MalformedURLException {
+		super(getJarUrl(workImage),JazminClassloader.class.getClassLoader());
+//		try {
+//			Class<?> claz = this.findClass("itf.dao.ITFDAO");
+//			System.err.println("xxxxxclaz:"+claz);
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
+	}
+	//
+	static void getJarFiles(List<URL>result,File dir) {
+		if(dir.isFile()&&dir.getAbsolutePath().endsWith(".jar")) {
+			try {
+				result.add(dir.toURI().toURL());
+				System.err.println("add "+dir.getAbsolutePath());
+			} catch (MalformedURLException e) {
+				logger.catching(e);
+			}
+		}
+		if(dir.isDirectory()) {
+			File children[]=dir.listFiles();
+			for(File e:children) {
+				JazminClassloader.getJarFiles(result,e);
+			}
+		}
+	}
+	//
+	static URL[] getJarUrl(File workImage) {
+		List<URL> urls=new ArrayList<>();
+		try {
+			urls.add(workImage.toURI().toURL());
+		} catch (MalformedURLException e) {
+			logger.error(e.getMessage(), e);
+		}
+		String expandPath=workImage.getAbsolutePath();
+		expandPath=expandPath.substring(0,expandPath.length()-4);
+		//
+		File expandLibFile=new File(expandPath+"/lib");
+		if(expandLibFile.exists()) {
+			getJarFiles(urls,expandLibFile);
+		}
+		//
+		logger.info("classpath urls:");
+		urls.forEach((u)->{
+			logger.info(u);
+		});
+		return urls.toArray(new URL[urls.size()]);
+	}
+
+}
